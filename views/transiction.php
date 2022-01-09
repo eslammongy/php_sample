fbfdb56a3e549b6eec819d721fa0464cf581d97b
eslam <eslammongy995@gmail.com>
2022-01-09 11:29:12 +0200

    initial commit

---
+++ views/transiction.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Transactions</title>
+    <style>
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            text-align: center;
+        }
+        table tr th, table tr td {
+            padding: 5px;
+            border: 1px #eee solid;
+        }
+        tfoot tr th, tfoot tr td {
+            font-size: 20px;
+        }
+        tfoot tr th {
+            text-align: right;
+        }
+    </style>
+</head>
+<body>
+
+<body>
+<table>
+    <thead>
+    <tr>
+        <th>Date</th>
+        <th>Check #</th>
+        <th>Description</th>
+        <th>Amount</th>
+    </tr>
+    </thead>
+    <tbody>
+    <!-- YOUR CODE -->
+    </tbody>
+    <tfoot>
+    <tr>
+        <th colspan="3">Total Income:</th>
+        <td><!-- YOUR CODE --></td>
+    </tr>
+    <tr>
+        <th colspan="3">Total Expense:</th>
+        <td><!-- YOUR CODE --></td>
+    </tr>
+    <tr>
+        <th colspan="3">Net Total:</th>
+        <td><!-- YOUR CODE --></td>
+    </tr>
+    </tfoot>
+</table>
+</body>
+
+</body>
+</html>
